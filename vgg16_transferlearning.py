e92171c8bfc7f8d78fd266ce8f7bba95664e7a2b
prakhar1602 <42375923+prakhar1602@users.noreply.github.com>
2021-04-18 21:30:36 -0400

    Adding vgg16_transferlearning.py

---
+++ vgg16_transferlearning.py
@@ -0,0 +1,28 @@
+from torch import nn
+from torchvision import models
+
+class VGG(nn.Module):
+    def __init__(self, num_classes, num_input = 4096):
+        super(VGG, self).__init__()
+        self.model = models.vgg16(pretrained=True)
+        self.num_classes = num_classes
+        self.num_input = num_input
+        
+        self.model.classifier[6] = nn.Sequential(
+            nn.Linear(self.num_input, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, self.num_classes),
+            nn.LogSoftmax(dim=1)
+            )
+        
+        
+    def create_model(self):
+        '''
+        Function to create a VGG16 model using transfer learning
+        '''
+        #freezing the pretrained layers
+        for param in self.model.parameters():
+            param.requires_grad = False
+            
+        return self.model
